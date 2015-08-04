490842fa78fccd2b78d43b67e2abea21e897b705
Elisandro Espindola <spark@Elisandros-MacBook-Pro.local>
2015-08-04 01:19:21 -0300

    defining callbacks

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+options = {
+	state: 1,
+	init: function(){
+		console.log('init config!');
+		
+	},
+};
+
+
+
--- index.js
+++ index.js
@@ -7,37 +7,56 @@ client.artist('Marcus Price', function(err, artist) {
 */
 
 
+/*
+
+resumo do funcionamento
+========================
+
+- lê as músicas e vai adicionando em um array
+	- futuramente vai tentar pegar os nomes pelo NOME do arquivo/folder
+- o array é lido e cada entrada vai sendo pedida ao Discogs
+- o array é lido e cada entrada vai sendo pedida ao lastFM
+- o array de respostas é lido inteiro para montar as tags de cada música, que será colocado em outro array
+	- para montar isso, tem que ler o array de  options, 'donotremovethese', 
+	  talvez alguma opção de mostre a priorização do merge, e com certeza um replace para uniformizar as tags/generos
+	- para usar as tags do last fm, talvez tenha que fazer um algoritmo mais complicado um pouco
+	- salvar cada música com as tags novas
+	
+
+*/
+
+require('./config.js');
+
+options.init();
+console.info('---- ', options, '----');
 
 
 var prefs = {
 
 
-    // Replace a discogs genre string with your own
-    replaceGenres: {
-        'Folk, World, & Country': 'Traditional',
-        'RnB/Swing': 'R&B'
-    },
+	// Replace a discogs genre string with your own
+	replaceGenres: {
+		'Folk, World, & Country': 'Traditional',
+		'RnB/Swing': 'R&B'
+	},
 
-    // The title of the album on discogs must not be more dissimilar in % to the title in itunes
-    matchThreshold: 35 //%
+	// The title of the album on discogs must not be more dissimilar in % to the title in itunes
+	matchThreshold: 35 //%
 };
 
 
 var showTags = function(){
-	console.log(sourcesTags);
+	console.log("tags: ", sourcesTags);
 };
+
 
 var Discogs2 = require('disconnect').Client;
 var db = new Discogs2({
-    consumerKey: 'srolJgNVDDStSniQPnTp', 
-    consumerSecret: 'RqpjuDxPGPIZerYleQeyrbWkTCZurEgG'
+	consumerKey: 'srolJgNVDDStSniQPnTp', 
+	consumerSecret: 'RqpjuDxPGPIZerYleQeyrbWkTCZurEgG'
 }).database();
 
-
 var taglib = require('taglib');
-
-
-
 
 var LastFmNode = require('lastfm').LastFmNode;
 
@@ -51,9 +70,13 @@ var lastfm = new LastFmNode({
 
 // general variables
 var track = "testsong.mp3";
+var tracks = ["testsong.mp3","testsong2.mp3"];
+
 
 var sourcesSource = {mbid: null, lastfm: {track: null, artist: null, album: null}, discogs:{track:{}}};
 var sourcesTags = false;
+
+/*
 // run them all individually
 var tag = taglib.tagSync(track);
  
@@ -129,20 +152,88 @@ var request = lastfm.request("artist.getTopTags", {
 
 
 db.search("", { artist: tag.artist, track: tag.title}, function(err, data){
-    // console.log(err, data);
-    
-    if(data.results.length > 0){
-    	console.log(' 4=== encontrou discogs');
-    	sourcesTags.discogs.track = data.results[0];
-    }
-    /*for(i=0; i<=data.results.length; i++){
-		
-		console.log(i, data.results[i]); 
-	}*/
+	// console.log(err, data);
+	
+	if(data.results.length > 0){
+		console.log(' 4=== encontrou discogs');
+		sourcesTags.discogs.track = data.results[0];
+	}
 
-    //console.log(err, data);
-    showTags();
+	showTags();
 });
+console.log(sourcesTags);
+
+
+*/
+
+
+
+function onReadMusicFile(err, tag, props){
+	console.dir(err ? err : {'tag': tag, 'audioProperties': props});
+	console.info("sourceTags: ",i, sourceTags);
+	// TODO: save image!
+	var request = lastfm.request("track.getTopTags", {
+		track: tag.title,
+		artist: tag.artist,
+		handlers: {
+			success: onGetLastFMInfo,
+			error: onConsultLastFMError
+		}
+	});
+}
+
+function onGetLastFMInfo(data){
+	if(data.track.mbid){
+		console.log(' 1=== encontrou track getInfo MBID lastfm');
+		sourcesTags.mbid = data.track.mbid;
+	}
+
+	var request = lastfm.request("track.getTopTags", {
+		track: tag.title,
+		artist: tag.artist,
+		handlers: {
+			success: onConsultLastFM,
+			error: onConsultLastFMError
+		}
+	});
+
+	
+//	db.search("", { artist: tag.artist, track: tag.title}, onConsultDiscogs);
+
+}
+
+function onConsultLastFM(data){
+	console.log(' 2=== encontrou track getInfo MBID lastfm');
+	sourcesTags.lastfm.track = data.toptags;
+//		console.log("UHU: " , data, data.toptags, data.length);
+	for(i=0; i<=data.length; i++){
+//				console.log(i, data[i]); 
+	}
+	showTags();
+
+	
+//	db.search("", { artist: tag.artist, track: tag.title}, onConsultDiscogs);
+
+}
+function onConsultLastFMError(err){
+	console.log("Error: " , err);
+
+}
+function onConsultDiscogs(err, data){
+	if(data.results.length > 0){
+		console.log(' 4=== encontrou discogs');
+		sourcesTags.discogs.track = data.results[0];
+	}
+
+	showTags();
+}
+
+
+var sourceTags = [];
+for (var i = 0; i < tracks.length; i++) {
+	sourceTags[i] = sourcesSource;
+	taglib.read(tracks[i], onReadMusicFile);
+}
 console.log(sourcesTags);
 
 /*
