1534ae219cd9058bae390761e940d662ee206654
Spark <djspark@gmail.com>
2015-08-12 00:09:03 -0300

    binding 'this' to the callback functions

--- track.js
+++ track.js
@@ -11,6 +11,13 @@ var lastfm = new LastFmNode({
 	useragent: 'appname/vX.X MyApp' // optional. defaults to lastfm-node.
 });
 
+var Discogs2 = require('disconnect').Client;
+var db = new Discogs2({
+	consumerKey: 'srolJgNVDDStSniQPnTp', 
+	consumerSecret: 'RqpjuDxPGPIZerYleQeyrbWkTCZurEgG'
+}).database();
+
+
 function Track(filename) {
 	this.filename = filename || "";
 	this.title; 
@@ -18,59 +25,98 @@ function Track(filename) {
 	this.artist; 	
 	this.genres = "";
 	this.comments = "";
-		this.filetag = {};
+	this.filetag = {};
 	this.fileprops = {};
+	this.discogsResults = {};
+	this.mbid = null;
 
 }
 
 Track.prototype.getFileInfo = function() {
 //	console.log('x', this, this.filename);
 
-	taglib.read(this.filename, this.getLastfmInfo);
+	taglib.read(this.filename, this.getLastfmInfo.bind(this));
 	// body...
 };
 Track.prototype.set = function() {
 	// body...
 };
-Track.prototype.getLastfmTags = function(data) {
-	if(data.track.mbid){
-		console.log(' 1=== encontrou track getInfo MBID lastfm');
-		sourcesTags.mbid = data.track.mbid;
-	}
-/*
-	var request = lastfm.request("track.getTopTags", {
-		track: tag.title,
-		artist: tag.artist,
-		handlers: {
-			success: this.getLastfmTags,
-			error: function(err) {
-	console.log("Error: " , err);
-}
-		}
-	});
-*/
-};
+
 Track.prototype.getLastfmInfo = function(err, tag, props){
 	//console.dir({'tag': tag, 'audioProperties': props});
 	this.filetag = tag;
 	this.fileprops = props;
-console.info(typeof this.getLastfmTags);
-//	console.log('y', this);
-	var request = lastfm.request("track.getTopTags", {
-		track: tag.title,
-		artist: tag.artist,
+
+	console.log('y', this);
+	var request = lastfm.request("track.getInfo", {
+		track: this.filetag.title,
+		artist: this.filetag.artist,
 		handlers: {
-			success: this.getLastfmTags,
-			error: function(err) {
-	console.log("Error: " , err);
-},
+			success: this.getLastfmTags.bind(this),
+			error: this.onConsultLastFMError.bind(this),
 		}
 	});
-	
+};
+
+Track.prototype.getLastfmTags = function(data) {
+	if(data.track.mbid){
+		console.log(' 1=== encontrou track getInfo MBID lastfm');
+		this.mbid = data.track.mbid;
+	}
+	var request = lastfm.request("track.getTopTags", {
+		track: this.filetag.title,
+		artist: this.filetag.artist,
+		handlers: {
+			success: this.getDiscogsInfo.bind(this),
+			error: this.onConsultLastFMError.bind(this),
+		}
+	});
 };
 
 Track.prototype.getDiscogsInfo = function(data) {
-	// body...
+	var self = this;
+	db.search("", { artist: this.filetag.artist, track: this.filetag.title}, function(err, data){
+		// console.log(err, data);
+		
+		if(data.results.length > 0){
+			console.log(' 4=== encontrou discogs');
+			console.info(data.results[0].style);
+			console.info(data.results[0]);
+			this.discogsResults = data;
+
+/*
+{ style: [ 'Hi NRG' ],
+  thumb: 'https://api-img.discogs.com/yIM9jA43voZxwooMc0_UpPTQOSE=/fit-in/150x150/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-297751-1261223355.jpeg.jpg',
+  format: [ 'Vinyl', '12"', '33 ⅓ RPM' ],
+  country: 'Canada',
+  barcode: [ 'BS-029 CRC' ],
+  uri: '/Divine-Shoot-Your-Shot/release/297751',
+  community: { want: 29, have: 23 },
+  label: 
+   [ 'Black Sun',
+     'Downstairs Records Ltd.',
+     '"O" Records',
+     'Unidisc Records Ltd.' ],
+  catno: 'BS-029',
+  year: '1982',
+  genre: [ 'Electronic' ],
+  title: 'Divine - Shoot Your Shot',
+  resource_url: 'https://api.discogs.com/releases/297751',
+  type: 'release',
+  id: 297751 }
+*/
+		}
+		self.chooseInfo();
+	});
+};
+
+Track.prototype.chooseInfo = function() {
+	console.log(' <--- choooose --->', this);
+	this.writeInfo();
+};
+
+Track.prototype.writeInfo = function() {
+	console.log(' <--- É O FIM --->', this);
 };
 
 Track.prototype.onConsultLastFMError = function(err) {
